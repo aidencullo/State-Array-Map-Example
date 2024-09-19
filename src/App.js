281cca6aae4fdbe16fe0c7cec791436dbaaf3e57
Aiden Cullo <aidencullo@gmail.com>
2024-09-19 14:16:48 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+let initialCounters = [0, 2, 0];
+
+export default function CounterList() {
+  const [counters, setCounters] = useState(initialCounters);
+
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // Set the other counters to 0
+        return 0;
+      }
+    });
+    setCounters(nextCounters);
+  }
+
+  return (
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button
+            onClick={() => {
+              handleIncrementClick(i);
+            }}
+          >
+            Increment
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
